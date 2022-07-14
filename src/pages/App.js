3d5cd437c4975515350f35b95fc2c6b9de6de2c9
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2022-07-14 20:51:01 +0700

    Add routing to home page

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,11 +2,13 @@ import { React } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Admin from './admin/Admin';
 import Login from './login/Login';
+import Home from './home/Home';
 export default function App() {
     return (
         <Routes>
             <Route path="admin/*" element={<Admin />} />
             <Route path="login" element={<Login />} />
+            <Route path="/" element={<Home />} />
         </Routes>
     );
 }
