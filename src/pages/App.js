6f4c17b5a836b0afd8d9a26944397b818385e414
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2022-07-14 16:28:39 +0700

    Add route to login page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,12 @@
 import { React } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Admin from './admin/Admin';
+import Login from './login/Login';
 export default function App() {
     return (
         <Routes>
             <Route path="admin/*" element={<Admin />} />
+            <Route path="login" element={<Login />} />
         </Routes>
     );
 }
