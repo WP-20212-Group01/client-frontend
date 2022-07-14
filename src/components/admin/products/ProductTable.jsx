b5080b1a8aba66041c6800a0e6e9c4d40441d4c0
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2022-07-14 11:43:19 +0700

    Change path

---
+++ src/components/admin/products/ProductTable.jsx
@@ -0,0 +1,188 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {
+    GridRowModes,
+    DataGrid,
+    GridToolbarContainer,
+    GridActionsCellItem,
+} from '@mui/x-data-grid';
+const initialRows = [
+    { id: 1, name: "Prouduct 1", price: "$10.00", stock: "10", category: "Category 1", status: "Selling" },
+    { id: 2, name: "Prouduct 2", price: "$20.00", stock: "20", category: "Category 2", status: "Selling" },
+    { id: 3, name: "Prouduct 3", price: "$30.00", stock: "30", category: "Category 3", status: "Selling" },
+    { id: 4, name: "Prouduct 4", price: "$40.00", stock: "40", category: "Category 4", status: "Selling" }
+]
+function EditToolbar(props) {
+    const { setRows, setRowModesModel } = props;
+
+    const handleClick = () => {
+        const id = 69;
+        setRows((oldRows) => [...oldRows, { id, name: '', price: '', stock: '', category: '', status: '', isNew: true }]);
+        setRowModesModel((oldModel) => ({
+            ...oldModel,
+            [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+        }));
+    };
+
+    return (
+        <GridToolbarContainer>
+            <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+                Add record
+            </Button>
+        </GridToolbarContainer>
+    );
+}
+
+EditToolbar.propTypes = {
+    setRowModesModel: PropTypes.func.isRequired,
+    setRows: PropTypes.func.isRequired,
+};
+
+export default function ProductTable() {
+    const [rows, setRows] = React.useState(initialRows);
+    const [rowModesModel, setRowModesModel] = React.useState({});
+
+    const handleRowEditStart = (params, event) => {
+        event.defaultMuiPrevented = true;
+    };
+
+    const handleRowEditStop = (params, event) => {
+        event.defaultMuiPrevented = true;
+    };
+
+    const handleEditClick = (id) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+    };
+
+    const handleSaveClick = (id) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    };
+
+    const handleCancelClick = (id) => () => {
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: GridRowModes.View, ignoreModifications: true },
+        });
+
+        const editedRow = rows.find((row) => row.id === id);
+        if (editedRow.isNew) {
+            setRows(rows.filter((row) => row.id !== id));
+        }
+    };
+
+    const processRowUpdate = (newRow) => {
+        const updatedRow = { ...newRow, isNew: false };
+        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+        return updatedRow;
+    };
+
+    const columns = [
+        {
+            field: 'name',
+            headerName: 'Name',
+            flex: 1,
+            editable: true
+        },
+        {
+            field: 'price',
+            headerName: 'Price',
+            type: 'string',
+            flex: 0.3,
+            editable: true
+        },
+        {
+            field: 'stock',
+            headerName: 'Stock',
+            type: 'number',
+            flex: 0.3,
+            editable: true,
+        },
+        {
+            field: 'category',
+            headerName: 'Category',
+            flex: 0.5,
+            editable: true,
+        },
+        {
+            field: 'status',
+            headerName: 'Status',
+            flex: 0.3,
+            editable: true,
+        },
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: 'Actions',
+            width: 100,
+            cellClassName: 'actions',
+            getActions: ({ id }) => {
+                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+                if (isInEditMode) {
+                    return [
+                        <GridActionsCellItem
+                            icon={<SaveIcon />}
+                            label="Save"
+                            onClick={handleSaveClick(id)}
+                        />,
+                        <GridActionsCellItem
+                            icon={<CancelIcon />}
+                            label="Cancel"
+                            className="textPrimary"
+                            onClick={handleCancelClick(id)}
+                            color="inherit"
+                        />,
+                    ];
+                }
+
+                return [
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEditClick(id)}
+                        color="inherit"
+                    />,
+                ];
+            },
+        },
+    ];
+
+    return (
+        <Box
+            sx={{
+                height: 500,
+                width: '100%',
+                '& .actions': {
+                    color: 'text.secondary',
+                },
+                '& .textPrimary': {
+                    color: 'text.primary',
+                },
+            }}
+        >
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                editMode="row"
+                rowModesModel={rowModesModel}
+                onRowEditStart={handleRowEditStart}
+                onRowEditStop={handleRowEditStop}
+                processRowUpdate={processRowUpdate}
+                components={{
+                    Toolbar: EditToolbar,
+                }}
+                componentsProps={{
+                    toolbar: { setRows, setRowModesModel },
+                }}
+                experimentalFeatures={{ newEditingApi: true }}
+            />
+        </Box>
+    );
+}
--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import OrderHistory from '../../components/admin/OrderHistory'
-import ProductTable from '../../components/admin/ProductTable'
+import OrderHistory from '../../components/admin/orders/OrderHistory'
+import ProductTable from '../../components/admin/products/ProductTable'
 import Topbar from '../../components/admin/Topbar'
 
 export default function Admin() {
