8a3e5f0e5c872e1cb952731282e26b4710319432
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2022-07-18 15:26:51 +0700

    Add routing to product page

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,12 +4,14 @@ import Admin from './admin/Admin';
 import Login from './login/Login';
 import Home from './home/Home';
 import Cart from './cart/Cart';
+import ProductPage from './home/ProductPage';
 export default function App() {
     return (
         <Routes>
             <Route path="admin/*" element={<Admin />} />
             <Route path="login" element={<Login />} />
             <Route path="/" element={<Home />} />
+            <Route path="product/:id" element={<ProductPage />} />
             <Route path="cart" element={<Cart />} />
         </Routes>
     );
