98d4a2787ba96982f559d00377f0385987f5c52a
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2022-07-19 22:46:43 +0700

    Add routing to register page

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,14 +5,18 @@ import Login from './login/Login';
 import Home from './home/Home';
 import Cart from './cart/Cart';
 import ProductPage from './home/ProductPage';
+import Register from './register/Register';
+import PurchaseHistory from './user/PurchaseHistory';
 export default function App() {
     return (
         <Routes>
             <Route path="admin/*" element={<Admin />} />
             <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
             <Route path="/" element={<Home />} />
             <Route path="product/:id" element={<ProductPage />} />
             <Route path="cart" element={<Cart />} />
+            <Route path="user/history" element={<PurchaseHistory />} />
         </Routes>
     );
 }
