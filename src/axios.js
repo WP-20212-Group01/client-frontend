4173119c177c9e1e94c65b74bf32527e30e97dff
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2022-07-24 23:17:47 +0700

    Add credential check

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,6 @@
 const axios = require('axios').default;
+const adminToken = localStorage.getItem('adminToken');
 axios.defaults.baseURL = 'https://wpnotwp.kcbtathust.tech/api';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Authorization'] = `Bearer ${adminToken}`;
 export default axios;
